43f3cd01fc878d174b7900b9442f37eb3247b9f2
nasir-sarkar <nasirsarkar537@gmail.com>
2025-04-08 12:23:19 +0600

    first commit

---
+++ JS/Discount_Entry.js
@@ -0,0 +1,19 @@
+function validate() {
+    let promo = document.getElementById("promo").value;
+    let promoError = document.getElementById("promoerror");
+    let isValid = true;
+
+    if (promo === "") {
+        promoError.innerHTML = "Promo code cannot be empty!";
+        promoError.style.color = "red";
+        isValid = false;
+    } else if (promo.length !== 5) {
+        promoError.innerHTML = "Promo code must be exactly 5 characters!";
+        promoError.style.color = "red";
+        isValid = false;
+    } else {
+        promoError.innerHTML = "";
+    }
+
+    return isValid;
+}
